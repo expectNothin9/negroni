7da79de2ed0b72ad5ff2d1fdb385078d9f654489
expectNothin9 <expect.nothin9@gmail.com>
2021-07-17 12:20:03 +0800

    fix: next lint, google fonts to _document.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/globals.css'
@@ -6,9 +5,6 @@ import '../styles/globals.css'
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet" />
-      </Head>
       <Component {...pageProps} />
     </Provider>
   )
