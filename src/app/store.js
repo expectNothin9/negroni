ccaa1ac8ececc2ca57b3257defc13870b7db4443
expectNothin9 <expect.nothin9@gmail.com>
2021-07-17 14:18:29 +0800

    fix: remove no-used counter slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import navReducer from '../features/navSlice'
 import pagesReducer from '../features/pagesSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     nav: navReducer,
     pages: pagesReducer
   }
