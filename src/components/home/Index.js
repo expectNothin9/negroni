300f6dc3880585d282e8fe787f80cf6a1a0f5b1a
expectNothin9 <expect.nothin9@gmail.com>
2021-07-18 13:24:36 +0800

    feat: change <Logo> and <Halo> to vmin size and tune colors

--- src/components/home/Index.js
+++ src/components/home/Index.js
@@ -11,19 +11,19 @@ const HaloBox = styled.div`
     left: 0;
     background-color: var(--secondary-dark);
     color: var(--on-secondary-dark);
-    font-size: 24px;
+    font-size: 5vmin;
     font-weight: bold;
     transform-origin: center;
     transform-style: preserve-3d;
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 28px;
-    height: 40px;
-    padding: 6px 0;
-    transform: rotateY(calc(var(--i) * calc(360deg / 28))) translateZ(120px) translateY(-56px);
-    border-top: 4px solid var(--secondary);
-    border-bottom: 4px solid var(--secondary);
+    width: 6vmin;
+    height: 7vmin;
+    transform: rotateY(calc(var(--i) * calc(360deg / 32))) translateZ(calc(23vmin + 6vmin))
+      translateY(20vmin);
+    border-top: 1vmin solid var(--secondary);
+    border-bottom: 1vmin solid var(--secondary);
   }
   span:before {
     content: '';
@@ -43,44 +43,40 @@ const HaloBox = styled.div`
       transform: perspective(1000px) rotateY(0deg) rotateX(15deg);
     }
   }
-  @media only screen and (max-width: 428px) {
-    span {
-      font-size: 20px;
-      width: 20px;
-      height: 36px;
-      transform: rotateY(calc(var(--i) * calc(360deg / 28))) translateZ(80px) translateY(-52px);
-    }
-  }
 `
 const HALO_LETTERS = [
-  { id: '1', char: 'A' },
-  { id: '2', char: 'G' },
-  { id: '3', char: 'E' },
-  { id: '4', char: 'D' },
-  { id: '5', char: 'L' },
-  { id: '6', char: 'I' },
-  { id: '7', char: 'O' },
-  { id: '8', char: 'N' },
-  { id: '9', char: '' },
-  { id: '10', char: '' },
+  { id: '1', char: '★' },
+  { id: '2', char: '' },
+  { id: '3', char: 'A' },
+  { id: '4', char: 'G' },
+  { id: '5', char: 'E' },
+  { id: '6', char: 'D' },
+  { id: '7', char: 'L' },
+  { id: '8', char: 'I' },
+  { id: '9', char: 'O' },
+  { id: '10', char: 'N' },
   { id: '11', char: '' },
-  { id: '12', char: '' },
+  { id: '12', char: '★' },
   { id: '13', char: '' },
   { id: '14', char: '' },
   { id: '15', char: '' },
   { id: '16', char: '' },
-  { id: '17', char: 'A' },
-  { id: '18', char: 'G' },
-  { id: '19', char: 'L' },
-  { id: '20', char: 'N' },
-  { id: '21', char: '' },
-  { id: '22', char: '' },
-  { id: '23', char: '' },
-  { id: '24', char: '' },
+  { id: '17', char: '' },
+  { id: '18', char: '' },
+  { id: '19', char: '★' },
+  { id: '20', char: '' },
+  { id: '21', char: 'A' },
+  { id: '22', char: 'G' },
+  { id: '23', char: 'L' },
+  { id: '24', char: 'N' },
   { id: '25', char: '' },
-  { id: '26', char: '' },
+  { id: '26', char: '★' },
   { id: '27', char: '' },
-  { id: '28', char: '' }
+  { id: '28', char: '' },
+  { id: '29', char: '' },
+  { id: '30', char: '' },
+  { id: '31', char: '' },
+  { id: '32', char: '' }
 ]
 const Halo = () => {
   return (
@@ -96,19 +92,18 @@ const Halo = () => {
 
 const LogoBox = styled.div`
   display: inline-block;
-  padding: 24px;
+  padding: 3vmin;
   border-radius: 50%;
-  background-color: var(--primary-light);
-  box-shadow: inset 0 0 20px var(--primary-dark), 0 0 50px var(--primary-light);
+  background-color: var(--surface);
+  box-shadow: 0 0 3vmin var(--shadow);
   img {
-    max-width: 60vmin;
+    width: 35vmin;
   }
   @media (prefers-reduced-motion: no-preference) {
     .logo {
       animation: logo-float infinite 3s ease-in-out;
     }
   }
-
   @keyframes logo-float {
     0% {
       transform: translateY(0);
