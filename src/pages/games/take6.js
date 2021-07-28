3276ca86b60734e662218896c1e7f53abb2aa26d
expectNothin9 <expect.nothin9@gmail.com>
2021-07-28 21:24:38 +0800

    feat: cookies 7days check in /games/take6

--- src/pages/games/take6.js
+++ src/pages/games/take6.js
@@ -7,35 +7,46 @@ import { getLineAuthorizeUrl } from '../../utils'
 import Main from '../../components/Main'
 import UniHeader from '../../components/UniHeader'
 import Take6Index from '../../components/games/Take6'
-// import { initGames } from '../../features/pageSlice'
-// import { fetchTable } from '../../features/asyncThunks'
+import { initGames } from '../../features/pageSlice'
+import { fetchTable } from '../../features/asyncThunks'
 
 const { COOKIES_KEY } = process.env
 
 export const getServerSideProps = nextReduxWrapper.getServerSideProps(
   (store) => async (context) => {
+    let needLogin = false
     const { req, res } = context
     const cookies = new Cookies(req, res, { keys: [COOKIES_KEY] })
     const t = cookies.get('t', { signed: true })
-    const cookiesDate = dayjs(t)
-    const now = dayjs()
-    console.log(`cookies.t: ${t}, now.diff(cookieDate, 'day'): ${now.diff(cookiesDate, 'day')}`)
-    // TODO: use userId, name if cookies date is within 7 days
-    return {
-      redirect: {
-        destination: getLineAuthorizeUrl('PLAY_TAKE6'),
-        statusCode: 302
+    console.log(`cookies.t: ${t}`)
+    if (!t) {
+      needLogin = true
+    } else {
+      const cookiesDate = dayjs(t)
+      const now = dayjs()
+      console.log(`now.diff(cookieDate, 'day'): ${now.diff(cookiesDate, 'day')}`)
+      if (now.diff(cookiesDate, 'day') > 7) {
+        needLogin = true
       }
     }
 
-    // store.dispatch(
-    //   initGames({
-    //     nav: {
-    //       activeTab: 'games'
-    //     }
-    //   })
-    // )
-    // await store.dispatch(fetchTable({ tableId: 1 }))
+    if (needLogin) {
+      return {
+        redirect: {
+          destination: getLineAuthorizeUrl('PLAY_TAKE6'),
+          statusCode: 302
+        }
+      }
+    }
+
+    store.dispatch(
+      initGames({
+        nav: {
+          activeTab: 'games'
+        }
+      })
+    )
+    await store.dispatch(fetchTable({ tableId: 1 }))
   }
 )
 
